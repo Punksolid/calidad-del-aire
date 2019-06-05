9974458fb737b547c7760235d5516cef49414ea0
Jose G. Rivera <josepablogr@gmail.com>
2019-06-04 23:33:05 -0600

    Footer & Tailwind Config

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-    plugins: {
-      tailwindcss: {},
-      'vue-cli-plugin-tailwind/purgecss': {},
-      autoprefixer: {}
-    }
-};
+    plugins: [
+        // ...
+        require('tailwindcss'),
+        require('autoprefixer'),
+        // ...
+    ]
+}
